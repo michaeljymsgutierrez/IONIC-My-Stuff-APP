b0dbac6f87c29126f46dd49471a31502da78b8fb
michaeljymsgutierrez <michael@vielsoft.com>
2016-07-27 16:54:57 +0800

    remove states from app.js

--- www/js/app.js
+++ www/js/app.js
@@ -36,29 +36,3 @@ var app = angular.module('starter', ['ionic','firebase'])
 
 
 
-//States
-
-app.config(function($stateProvider,$urlRouterProvider){
-  
-  
-  $stateProvider
-  .state('signIn',{
-    
-      url: '/signIn',
-      controller: 'signInCtrl',
-      templateUrl: 'templates/login.html'
-
-  })
-  .state('signUp',{
-      url: '/signUp',
-      controller: 'signInCtrl',
-      templateUrl: 'templates/signup.html'
-  })
-  .state('todo',{
-      url: '/todo',
-      controller: 'todoCtrl',
-      templateUrl: 'templates/todo-tabs.html'
-  })
-  
-  $urlRouterProvider.otherwise('/signIn');
-});
