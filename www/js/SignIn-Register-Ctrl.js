cdbe0cd1cac2d8bb8752a089403505e51b609640
michaeljymsgutierrez <michael@vielsoft.com>
2016-07-29 14:40:22 +0800

    updated

--- www/js/SignIn-Register-Ctrl.js
+++ www/js/SignIn-Register-Ctrl.js
@@ -1,18 +1,12 @@
 
 //SigIn Controller
-app.controller('signInCtrl',function($scope,$location,$firebaseAuth){
+app.controller('signInCtrl',function($scope,$location){
   
     //function signin
     $scope.login = function(userlog,passlog) {
      
         console.log('Log In Clicked');
         
-        firebase.auth().signInWithEmailAndPassword(userlog, passlog).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        });
 
 
     }
@@ -34,36 +28,8 @@ app.controller('signInCtrl',function($scope,$location,$firebaseAuth){
         console.log('Register Clicked');
         
             
-            console.log("User: "+user+"  Pass: "+pass);
-            var ref = new Firebase('https://mystuff-d12d5.firebaseio.com/');
-            ref.createUser({
-            email    : user,
-            password : pass
-            }, function(error, userData) {
-            if (error) {
-                console.log("Error creating user:", error);
-            } else {
-                console.log("Successfully created user account with uid:", userData.uid);
-            }
-            });
-                    
-        /*        
-        var fbAuth = $firebaseAuth(fb);
-        
-        fbAuth.$createUser({
-             email: username, 
-             password: password
-        }).then(function() {
-            return fbAuth.$authWithPassword({
-                email: username,
-                password: password
-            });
-        }).then(function(authData) {
-            $location.path('/signIn')
-        }).catch(function(error) {
-            console.error("ERROR " + error);
-        });
-        */
+
+
       
       
     } ;
--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic','firebase'])
+var app = angular.module('starter', ['ionic','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,7 +22,7 @@ var app = angular.module('starter', ['ionic','firebase'])
     }
    
    
-   //fb = new Firebase('https://mystuff-d12d5.firebaseio.com/');
+
   });
 })
 
