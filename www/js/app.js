5386d2af31403512a07e9b9761e1ad3cd84f1769
michaeljymsgutierrez <michael@vielsoft.com>
2016-08-01 10:05:00 +0800

    initialize db

--- www/js/app.js
+++ www/js/app.js
@@ -4,9 +4,13 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 var app = angular.module('starter', ['ionic','ngCordova'])
-
-.run(function($ionicPlatform) {
+ 
+var db = null;
+app.run(function($ionicPlatform,$cordovaSQLite) {
   $ionicPlatform.ready(function() {
+    
+
+    
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
@@ -19,9 +23,30 @@ var app = angular.module('starter', ['ionic','ngCordova'])
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
+      
+      
     }
-   
-   
+    
+    
+    // SQLite
+    
+        try {
+            db = $cordovaSQLite.openDB({name:"mystuff_db",location:'default'});
+            console.log('Sucessful openDB');
+        } catch (error) {
+            console.log('Error openDB');
+        }
+        
+        try{
+            $cordovaSQLite.execute(db, 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, uname TEXT, upass TEXT )');
+            console.log('Sucessful Create Table')
+        }catch(error){
+          console.log('Error executeDb');
+        }
+      
+
+
+    
 
   });
 })
